20a6fb84bbc38b054d4126febc6380bba63654a7
Bruno Baptista <brunobat@gmail.com>
2019-09-26 12:26:02 +0100

    remove entity

--- src/main/java/org/acme/legume/resource/LegumeResource.java
+++ src/main/java/org/acme/legume/resource/LegumeResource.java
@@ -2,18 +2,17 @@ package org.acme.legume.resource;
 
 import org.acme.legume.data.LegumeItem;
 import org.acme.legume.data.LegumeNew;
-import org.acme.legume.model.Legume;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static javax.ws.rs.core.Response.Status.CREATED;
@@ -23,7 +22,7 @@ import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 @ApplicationScoped
 public class LegumeResource implements LegumeApi {
 
-    private Map<String, Legume> repository = new ConcurrentHashMap<>();
+    private Map<String, LegumeItem> repository = new ConcurrentHashMap<>();
 
     public Response provision() {
         final LegumeNew carrot = LegumeNew.builder()
@@ -53,37 +52,23 @@ public class LegumeResource implements LegumeApi {
     }
 
     public List<LegumeItem> list() {
-        return repository.values().stream()
-                .map(legume -> LegumeItem.builder()
-                        .id(legume.getId())
-                        .name(legume.getName())
-                        .description(legume.getDescription())
-                        .build())
-                .collect(Collectors.toList());
+        return new ArrayList<>(repository.values());
     }
 
-    private Optional<Legume> find(final String legumeId) {
+    private Optional<LegumeItem> find(final String legumeId) {
         return Optional.ofNullable(repository.get(legumeId));
     }
 
     private LegumeItem addLegume(final @Valid LegumeNew legumeNew) {
         final String id = UUID.randomUUID().toString();
 
-        final Legume legumeToAdd = Legume.builder()
+        final LegumeItem legumeToAdd = LegumeItem.builder()
                 .id(id)
                 .name(legumeNew.getName())
                 .description((legumeNew.getDescription()))
                 .build();
 
         repository.put(id, legumeToAdd);
-        final Legume addedLegume = repository.get(id);
-
-        final LegumeItem legumeItem = LegumeItem.builder()
-                .id(addedLegume.getId())
-                .name(addedLegume.getName())
-                .description(addedLegume.getDescription())
-                .build();
-
-        return legumeItem;
+        return repository.get(id);
     }
 }
